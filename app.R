2ef669faf6332ab7300e48b00d4cdb81654285b1
Yifu Yan <yanyifu94@hotmail.com>
2018-09-06 21:41:30 -0700

    update app to add download button

--- app.R
+++ app.R
@@ -14,11 +14,14 @@ ui <- fluidPage(
    sidebarLayout(
        
        sidebarPanel(width = 4,
-           
+           #Input
            fileInput(inputId = "input_image",
-                     label = "Upload your image",
+                     label = "Upload Your Image",
                      placeholder = "Upload your image",
                      accept = c('image/png','image/jpeg')),
+           # textInput(inputId = "image_url",
+           #           label = "Or Input Your Image URL",
+           #           placeholder = "Image Url"),
            #ui to control shrinkage
            sliderInput(inputId = "shrinkage",
                        label = "% Pixels",
@@ -79,18 +82,23 @@ ui <- fluidPage(
                        max = 200,
                        value = 100,
                        step = 1)
-           
            ),
        
-       mainPanel(imageOutput("output_image"))
+       mainPanel(downloadButton("download_image","Download Image"),
+                 imageOutput("output_image")
+                 # tags$br(),
+                 # tags$h4("Current Palette"),
+                 # imageOutput("my_palette"))
    )
    
-)
+))
 
 server <- function(input, output) {
-    #observe upload button
+    #observe upload button or URL input box ------------
     image <- eventReactive(input$input_image,
                            image_convert(image_read(input$input_image$datapath),format = "png"))
+    # image <- eventReactive(input$image_url,
+    #                        image_convert(image_read(input$image_url),format = "png"))
     #get image info once uploaded
     your_image_info <- eventReactive(input$input_image,
                                 image_info(image()))
@@ -113,25 +121,36 @@ server <- function(input, output) {
                                           dplyr::filter(palette_name == input$palette) %>%
                                           pull(img_path))
     map_palette <- reactive(image_read(map_palette_path()))
+    output$my_palette <- renderImage({
+        tempfile1 <- reactive(image_write(map_palette(),tempfile(fileext = "png")))
+        return(list(src=tempfile1(),contentType="image/png"))
+    })
+    
+    modified_image <- reactive(image() %>%
+                                        image_modulate(brightness = input$brightness,
+                                                       saturation = input$saturation,
+                                                       hue = input$hue) %>%
+                                        image_contrast(sharpen = input$sharpen) %>%
+                                        image_blur(radius = input$blur_radius,
+                                                   sigma = input$blur_sigma) %>%
+                                        image_map(map = map_palette()) %>%
+                                        image_scale(target_size()) %>%
+                                        image_scale(image_size())
+                                    )
     
     output$output_image <- renderImage({
-        req(input$input_image)
-        
-        modified_image <- reactive(
-            image() %>%
-                image_modulate(brightness = input$brightness,
-                               saturation = input$saturation,
-                               hue = input$hue) %>%
-                image_contrast(sharpen = input$sharpen) %>%
-                image_blur(radius = input$blur_radius,
-                           sigma = input$blur_sigma) %>%
-                image_map(map = map_palette()) %>%
-                image_scale(target_size()) %>%
-                image_scale(image_size())
-            )
         tmpfile <- reactive(image_write(modified_image(),tempfile(fileext = "png")))
         return(list(src=tmpfile(),contentType="image/png"))
         })
+    
+    output$download_image <- downloadHandler(
+        filename = function(){
+            return(str_c("pixel_",input$input_image$name))
+            },
+        content = function(file){
+            return(image_write(modified_image(),file))
+            },
+        contentType = "image/png")
 }
 
 # Run the application 
